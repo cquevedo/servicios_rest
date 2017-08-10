76fd4ba606378bf8044cf2603001e8f2c72c4983
Cesar Quevedo <cquevedo@crossnet.ws>
2017-08-10 15:21:05 -0500

    [2] : Creación inicial de un servicio web

--- work_1/src/main/java/servicio/demo/servicio/ClienteService.java
+++ work_1/src/main/java/servicio/demo/servicio/ClienteService.java
@@ -1,7 +1,21 @@
 package servicio.demo.servicio;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
+
+@Path("/")
+@Produces({ "application/json" })
+@Consumes({ "application/json" })
 public class ClienteService {
 
+	@GET
+	@Path("/clientes")
+	public Response consultarClientes(){
+		return Response.ok("Hola Mundo").build();
+	}
 
 }
