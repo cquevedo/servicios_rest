0077c8950649a9a254b9a7cc51d0d2973f403f57
Cesar Quevedo <cquevedo@crossnet.ws>
2017-08-10 16:04:54 -0500

    [3] : Implementación seguridad servicio rest

--- work_1/src/main/java/servicio/demo/servicio/ClienteService.java
+++ work_1/src/main/java/servicio/demo/servicio/ClienteService.java
@@ -1,5 +1,6 @@
 package servicio.demo.servicio;
 
+import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -10,6 +11,7 @@ import javax.ws.rs.core.Response;
 @Path("/")
 @Produces({ "application/json" })
 @Consumes({ "application/json" })
+@RolesAllowed("restuser")
 public class ClienteService {
 
 	@GET
